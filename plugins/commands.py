c11eee91d61214a77e2e05a36b7a996f59ec6185
Techify Bots <RahulReviewsYT@gmail.com>
2025-01-10 23:29:51 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,112 @@
+import asyncio 
+from pyrogram import Client, filters, enums
+from config import LOG_CHANNEL, API_ID, API_HASH, NEW_REQ_MODE
+from plugins.database import db
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+LOG_TEXT = """<b>#NewUser
+    
+ID - <code>{}</code>
+
+Name - {}</b>
+"""
+
+async def get_fsub(bot, message):
+    target_channel_id = AUTH_CHANNEL  # Your channel ID
+    user_id = message.from_user.id
+    try:
+        # Check if user is a member of the required channel
+        await bot.get_chat_member(target_channel_id, user_id)
+    except UserNotParticipant:
+        # Generate the channel invite link
+        channel_link = (await bot.get_chat(target_channel_id)).invite_link
+        join_button = InlineKeyboardButton("🔔 Join Our Channel", url=channel_link)
+
+        # Display a message encouraging the user to join
+        keyboard = [[join_button]]
+        await message.reply(
+            f"<b>👋 Hello {message.from_user.mention()}, Welcome!</b>\n\n"
+            "📢 <b>Exclusive Access Alert!</b> ✨\n\n"
+            "To unlock all the amazing features I offer, please join our updates channel. "
+            "This helps us keep you informed and ensures top-notch service just for you! 😊\n\n"
+            "<i>🚀 Join now and dive into a world of knowledge and creativity!</i>",
+            reply_markup=InlineKeyboardMarkup(keyboard),
+        )
+        return False
+    else:
+        return True
+
+
+@Client.on_message(filters.command('start'))
+async def start_message(c,m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id, m.from_user.first_name)
+        await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
+    is_subscribed = await get_fsub(c, m)
+    if not is_subscribed:
+        return
+
+   await m.reply_text(f"{m.from_user.mention},\n\n𝖨 𝖼𝖺𝗇 𝖺𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝖼𝖺𝗅𝗅𝗒 𝖺𝗉𝗉𝗋𝗈𝗏𝖾 𝗎𝗌𝖾𝗋𝗌 𝗂𝗇 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌.\n\n𝖩𝗎𝗌𝗍 𝖺𝖽𝖽 𝗆𝖾 𝗂𝗇 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌 𝗐𝗂𝗍𝗁 𝗉𝖾𝗋𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝗍𝗈 𝖺𝖽𝖽 𝗇𝖾𝗐 𝗆𝖾𝗆𝖻𝖾𝗋𝗌.\n\n**<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/CallOwnerBot'>ʀᴀʜᴜʟ</a></blockquote>**",
+        reply_markup=InlineKeyboardMarkup(
+            [[
+                InlineKeyboardButton("⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆", url=f"https://telegram.me/QuickAcceptBot?startgroup=true&admin=invite_users"),
+            ],[
+                InlineKeyboardButton("• ᴜᴩᴅᴀᴛᴇꜱ •", url="https://telegram.me/StreamTales"),
+                InlineKeyboardButton("• ꜱᴜᴩᴩᴏʀᴛ •", url="https://telegram.me/TechifySupport")
+            ],[
+              InlineKeyboardButton("⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⇆", url=f"https://telegram.me/QuickAcceptBot?startchannel=true&admin=invite_users")
+            ]]
+        )
+    )
+
+@Client.on_message(filters.command('accept') & filters.private)
+async def accept(client, message):
+    show = await message.reply("**Please Wait.....**")
+    user_data = await db.get_session(message.from_user.id)
+    if user_data is None:
+        await show.edit("**For Accepte Pending Request You Have To /login First.**")
+        return
+    try:
+        acc = Client("joinrequest", session_string=user_data, api_hash=API_HASH, api_id=API_ID)
+        await acc.connect()
+    except:
+        return await show.edit("**Your Login Session Expired. So /logout First Then Login Again By - /login**")
+    show = await show.edit("**Now Forward A Message From Your Channel Or Group With Forward Tag\n\nMake Sure Your Logged In Account Is Admin In That Channel Or Group With Full Rights.**")
+    vj = await client.listen(message.chat.id)
+    if vj.forward_from_chat and not vj.forward_from_chat.type in [enums.ChatType.PRIVATE, enums.ChatType.BOT]:
+        chat_id = vj.forward_from_chat.id
+        try:
+            info = await acc.get_chat(chat_id)
+        except:
+            await show.edit("**Error - Make Sure Your Logged In Account Is Admin In This Channel Or Group With Rights.**")
+    else:
+        return await message.reply("**Message Not Forwarded From Channel Or Group.**")
+    await vj.delete()
+    msg = await show.edit("**Accepting all join requests... Please wait until it's completed.**")
+    try:
+        while True:
+            await acc.approve_all_chat_join_requests(chat_id)
+            await asyncio.sleep(1)
+            join_requests = [request async for request in acc.get_chat_join_requests(chat_id)]
+            if not join_requests:
+                break
+        await msg.edit("**Successfully accepted all join requests.**")
+    except Exception as e:
+        await msg.edit(f"**An error occurred:** {str(e)}")
+
+@Client.on_chat_join_request(filters.group | filters.channel)
+async def approve_new(client, m):
+    if NEW_REQ_MODE == False:
+        return 
+    try:
+        if not await db.is_user_exist(m.from_user.id):
+            await db.add_user(m.from_user.id, m.from_user.first_name)
+            await client.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
+        await client.approve_chat_join_request(m.chat.id, m.from_user.id)
+        try:
+            await client.send_message(m.from_user.id, "{},\n\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝗌𝗍 𝖳𝗈 𝖩𝗈𝗂𝗇 {} 𝖺𝗌 𝖻𝖾𝖾𝗇 𝖠𝖼𝖼𝖾𝗉𝗍𝖾𝖽.".format(m.from_user.mention, m.chat.title))
+        except:
+            pass
+    except Exception as e:
+        print(str(e))
+        pass
