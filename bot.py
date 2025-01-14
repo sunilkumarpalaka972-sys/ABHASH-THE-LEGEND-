0af38b368b0cd8659b6e893401961c9aea8e966b
Techify Bots <TechifyBots@gmail.com>
2025-01-14 11:23:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
 from pyrogram import Client
-from pyromod import listen
 from aiohttp import web
 from config import API_ID, API_HASH, BOT_TOKEN
 
+# Define routes for aiohttp web server
 r = web.RouteTableDef()
 
 @r.get("/", allow_head=True)
@@ -16,35 +16,35 @@ async def wsrvr():
     return wa
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-        "auto approve bot",
-         api_id=API_ID,
-         api_hash=API_HASH,
-         bot_token=BOT_TOKEN,
-         plugins=dict(root="plugins"),
-         workers=50,
-         sleep_threshold=10
+            "auto_approve_bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="plugins"),
+            workers=50,
+            sleep_threshold=10
         )
 
-
     async def start(self):
+        # Start aiohttp web server
         app = web.AppRunner(await wsrvr())
         await app.setup()
         ba = "0.0.0.0"
         port = int(os.environ.get("PORT", 8080)) or 8080
         await web.TCPSite(app, ba, port).start()
+        
+        # Start Pyrogram client
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
 
         print('Bot Started Powered By @TechifyBots')
 
-
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
+# Run the bot
 Bot().run()
