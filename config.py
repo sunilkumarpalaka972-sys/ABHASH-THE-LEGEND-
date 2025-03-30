8e56bf477d347a42158d1fdd02f914e2df4a543a
Techify Bots <TechifyBots@gmail.com>
2025-03-30 08:36:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,11 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 ADMINS = int(environ.get("ADMINS", ""))
-
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "autoacceptbot")
-
-# If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
+IS_FSUB = bool(os.environ.get("FSUB", True))
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "")
+AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
