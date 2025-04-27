652f0609db3146151841dcb9d48508fa16b01944
Techify Bots <TechifyBots@gmail.com>
2025-04-27 22:48:09 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,50 +1,33 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import AUTH_CHANNELS, ADMIN
-from pyrogram.errors import RPCError
+from config import AUTH_CHANNELS
+from pyrogram import Client
+from pyrogram.types import Message
+from typing import List
+from pyrogram.errors import UserNotParticipant
 
-
-async def get_fsub(client, message):
-    rahul = await client.get_me()
-    bot = client
+async def get_fsub(bot: Client, message: Message) -> bool:
+    tb = await bot.get_me()
     user_id = message.from_user.id
-    not_joined = []
-
+    not_joined_channels = []
     for channel_id in AUTH_CHANNELS:
         try:
-            member = await bot.get_chat_member(channel_id, user_id)
-            if member.status == "kicked":
-                await message.reply("**🚫 You are banned from using this bot**",
-                                    reply_markup=InlineKeyboardMarkup(
-                                        [[InlineKeyboardButton("Support", user_id=int(ADMIN))]]
-                                    ))
-                return False
-            if member.status in ["left", "restricted"]:
-                not_joined.append(channel_id)
-        except RPCError:
-            not_joined.append(channel_id)
-
-    if not not_joined:
-        return True
-
-    buttons = []
-    for index, channel_id in enumerate(not_joined, 1):
-        try:
+            await bot.get_chat_member(channel_id, user_id)
+        except UserNotParticipant:
             chat = await bot.get_chat(channel_id)
-            channel_link = chat.invite_link
-            if not channel_link:
-                raise ValueError("No invite link available")
-            buttons.append(InlineKeyboardButton(f"🔰 Channel {index} 🔰", url=channel_link))
-        except Exception as e:
-            print(f"Error fetching channel data: {e}")
-    tybutton = InlineKeyboardButton("🔄 Try Again", url=f"https://telegram.me/{rahul.username}?start=start")
-    buttons.append(tybutton)
-    formatted_buttons = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
-
-    await message.reply(
-        f"{message.from_user.mention}, To use the bot, you must join our channel first."
-        "The bot will not process any requests without joining.\n\n"
-        "बॉट का उपयोग करने के लिए आपको पहले हमारे चैनल में Join होना होगा। "
-        "बॉट बिना शामिल हुए किसी भी Request को Process नहीं करेगा।",
-        reply_markup=InlineKeyboardMarkup(formatted_buttons))
-    return False
-            
+            invite_link = chat.invite_link or await bot.export_chat_invite_link(channel_id)
+            not_joined_channels.append((chat.title, invite_link))
+    if not_joined_channels:
+        join_buttons = []
+        for i in range(0, len(not_joined_channels), 2):
+            row = []
+            for j in range(2):
+                if i + j < len(not_joined_channels):
+                    title, link = not_joined_channels[i + j]
+                    button_text = f"{i + j + 1}. {title}"
+                    row.append(InlineKeyboardButton(button_text, url=link))
+            join_buttons.append(row)
+        join_buttons.append([InlineKeyboardButton("🔄 Try Again", url=f"https://telegram.me/{tb.username}?start=start")])
+        await message.reply(f"**🎭 {message.from_user.mention}, As I see, you haven’t joined my channel yet.\nPlease join by clicking the button below.**",
+                            reply_markup=InlineKeyboardMarkup(join_buttons),)
+        return False
+    return True
