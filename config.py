a4fc14c23fc407f3f2186ebcebaaa00ee39e75d8
Techify Bots <TechifyBots@gmail.com>
2025-05-03 17:54:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
-from os import environ
+import os
 from typing import List
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "autoacceptbot")
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
-IS_FSUB = bool(environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "")
+
+IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
