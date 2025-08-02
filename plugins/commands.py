fd771c627db896640fcfa9a72e132b0330eb894d
Techify Bots <TechifyBots@gmail.com>
2025-08-02 22:51:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,9 +1,9 @@
-import asyncio
 import random
 from pyrogram import Client, filters, enums
 from pyrogram.errors import *
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
+import asyncio
 from Script import text
 from .db import tb
 from .fsub import get_fsub
@@ -35,32 +35,19 @@ async def start_cmd(client, message):
             ])
         )
 
-@Client.on_message(filters.command("stats") & filters.private & filters.user(ADMIN))
-async def total_users(client, message):
-    try:
-        users = await tb.get_all_users()
-        await message.reply(f"👥 **Total Users:** {len(users)}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎭 Close", callback_data="close")]]))
-    except Exception as e:
-        r = await message.reply(f"❌ *Error:* `{str(e)}`")
-        await asyncio.sleep(30)
-        await r.delete()
-
 @Client.on_message(filters.command('accept') & filters.private)
 async def accept(client, message):
     show = await message.reply("**Please Wait.....**")
     user_data = await tb.get_session(message.from_user.id)
     if user_data is None:
         return await show.edit("**To accept join requests, please /login first.**")
-
     try:
         acc = Client("joinrequest", session_string=user_data, api_id=API_ID, api_hash=API_HASH)
         await acc.connect()
     except:
         return await show.edit("**Your login session has expired. Use /logout first, then /login again.**")
-
     await show.edit("**Forward a message from your Channel or Group (with forward tag).\n\nMake sure your logged-in account is an admin there with full rights.**")
     fwd_msg = await client.listen(message.chat.id)
-
     if fwd_msg.forward_from_chat and fwd_msg.forward_from_chat.type not in [enums.ChatType.PRIVATE, enums.ChatType.BOT]:
         chat_id = fwd_msg.forward_from_chat.id
         try:
@@ -69,7 +56,6 @@ async def accept(client, message):
             return await show.edit("**Error: Ensure your account is admin in this Channel/Group with required rights.**")
     else:
         return await message.reply("**Message not forwarded from a valid Channel/Group.**")
-
     await fwd_msg.delete()
     msg = await show.edit("**Accepting all join requests... Please wait.**")
     try:
@@ -82,7 +68,6 @@ async def accept(client, message):
         await msg.edit("**✅ Successfully accepted all join requests.**")
     except Exception as e:
         await msg.edit(f"**An error occurred:** `{str(e)}`")
-
 
 @Client.on_chat_join_request()
 async def approve_new(client, m):
