120a30d6d14ee0c86daa319a5f7a7281c3a4ce29
sunilkumarpalaka972-sys <sunilkumarpalaka972@gmail.com>
2025-10-15 00:17:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://sunilkumarpalaka972_db_user:e4ZYD1IeAGg7vvi1@cluster0.j8z7o9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "sunilkumarpalaka972_db_user")
 
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
+IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "fales"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1003114110211").split())) # Add Multiple channel ids
