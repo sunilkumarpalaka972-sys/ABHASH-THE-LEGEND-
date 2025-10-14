cfb764fee9ce7bf9ff13d3f56af2da4b837cb973
sunilkumarpalaka972-sys <sunilkumarpalaka972@gmail.com>
2025-10-15 00:54:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8313516165:AAEevLh-17tE7l5yqXa8K_4qsvGZ
 ADMIN = int(os.environ.get("ADMIN", "8127363932"))
 PICS = (os.environ.get("PICS", "")).split()
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1003064804454"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003064804454"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://sunilkumarpalaka972_db_user:e4ZYD1IeAGg7vvi1@cluster0.j8z7o9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
