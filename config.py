a9267c5bc7242dd99cd93b5aceb01eaf461bd9c4
sunilkumarpalaka972-sys <sunilkumarpalaka972@gmail.com>
2025-10-15 23:36:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ PICS = (os.environ.get("PICS", "")).split()
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003064804454"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://sunilkumarpalaka972_db_user:e4ZYD1IeAGg7vvi1@cluster0.j8z7o9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "sunilkumarpalaka972_db_user")
 
 IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "fales"  # Set "True" For Enable Force Subscribe
